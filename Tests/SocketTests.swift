ac556b1789f29bd6d30928c4727b802cc494c131
Karl Weinmeister <kweinmeister@us.ibm.com>
2016-07-27 19:30:08 -0700

    Created a simple Socket test that creates, listens, and closes the socket, ensuring that the state variables are as expected.

--- Tests/SocketTests.swift
+++ Tests/SocketTests.swift
@@ -8,6 +8,12 @@
 
 import XCTest
 
+#if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+    import Darwin
+#elseif os(Linux)
+    import Glibc
+#endif
+
 @testable import Socket
 
 class SocketTests: XCTestCase {
@@ -22,18 +28,32 @@ class SocketTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-		/*
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testSocket() {
+        do {
+            let port:Int32 = 1337
+            
+            let socket = try Socket.create()
+            XCTAssertNotNil(socket)
+            XCTAssertFalse(socket.isConnected)
+            XCTAssertTrue(socket.isBlocking)
+            
+            try socket.listen(on: Int(port), maxBacklogSize: 10)
+            XCTAssertTrue(socket.isListening)
+            XCTAssertEqual(socket.listeningPort, port)
+            
+            socket.close()
+            XCTAssertFalse(socket.isActive)
+            
+        } catch let error {
+            
+            // See if it's a socket error or something else...
+            guard let socketError = error as? Socket.Error else {
+                
+                print("Unexpected error...")
+                return
+            }
+            
+            print("Error reported: \(socketError.description)")
         }
-		*/
     }
-    
 }
