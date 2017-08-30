b13217e33c7434da21292f562832ee6e22d9d8c1
Quan Vo <quan.vo@ibm.com>
2017-08-30 13:55:26 -0500

    Add Swift 4 support

--- Package.swift
+++ Package.swift
@@ -1,54 +1,34 @@
-// swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 //
 //  BlueSocket.swift
 //  BlueSocket
 //
-//  Copyright © 2017 IBM. All rights reserved.
+//  Copyright © 2016 IBM. All rights reserved.
 //
-// 	Licensed under the Apache License, Version 2.0 (the "License");
-// 	you may not use this file except in compliance with the License.
-// 	You may obtain a copy of the License at
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
 //
-// 	http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// 	Unless required by applicable law or agreed to in writing, software
-// 	distributed under the License is distributed on an "AS IS" BASIS,
-// 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// 	See the License for the specific language governing permissions and
-// 	limitations under the License.
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
 //
 
 import PackageDescription
 
 #if os(Linux) || os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-
+    
 let package = Package(
     name: "Socket",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "Socket",
-            targets: ["Socket"]),
-        ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target defines a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Socket",
-            dependencies: []),
-        .testTarget(
-            name: "SocketTests",
-            dependencies: ["Socket"]),
-        ]
+    targets: [Target(name: "Socket")],
+    exclude: ["BlueSocket.xcodeproj", "BlueSocket.xcworkspace", "README.md", "Sources/Info.plist", "Sources/Socket.h"]
 )
-
+    
 #else
-
+    
 fatalError("Unsupported OS")
-
+    
 #endif
