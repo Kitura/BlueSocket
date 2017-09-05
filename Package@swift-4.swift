feb412914e61510c8034ed2a2d70561d3571ff6f
Quan Vo <quan.vo@ibm.com>
2017-09-05 09:28:18 -0500

    Add excludes in Swift 4 package manifest

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -39,7 +39,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Socket",
-            dependencies: []),
+            dependencies: [],
+            exclude: ["BlueSocket.xcodeproj", "BlueSocket.xcworkspace", "README.md", "Sources/Info.plist", "Sources/Socket.h"]),
         .testTarget(
             name: "SocketTests",
             dependencies: ["Socket"]),
