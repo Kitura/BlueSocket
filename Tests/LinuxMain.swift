524266e308b77ac00081ac58bbb650a25a231bd6
Bill Abt <babt@me.com>
2018-04-26 14:40:55 -0400

    Fix for deprecation of IndexDistance in Swift 4.1.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -21,7 +21,7 @@ import Glibc
 
 // http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
 
-#if swift(>=4)
+#if swift(>=4.1)
 
     extension MutableCollection {
         mutating func shuffle() {
@@ -40,20 +40,41 @@ import Glibc
 
 #else
 
-    extension MutableCollection where Indices.Iterator.Element == Index {
-        mutating func shuffle() {
-            let c = count
-            guard c > 1 else { return }
+	#if swift(>=4)
 
-            srand(UInt32(time(nil)))
-            for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-				let d: Int = numericCast(random() % numericCast(unshuffledCount))
-                guard d != 0 else { continue }
-                let i = index(firstUnshuffled, offsetBy: d)
-                swap(&self[firstUnshuffled], &self[i])
-            }
-        }
-    }
+		extension MutableCollection {
+			mutating func shuffle() {
+			let c = count
+			guard c > 1 else { return }
+
+			srand(UInt32(time(nil)))
+			for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+				let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
+				guard d != 0 else { continue }
+				let i = index(firstUnshuffled, offsetBy: d)
+				swapAt(firstUnshuffled, i)
+			}
+		}
+	}
+
+	#else
+
+    	extension MutableCollection where Indices.Iterator.Element == Index {
+        	mutating func shuffle() {
+            	let c = count
+            	guard c > 1 else { return }
+
+	            srand(UInt32(time(nil)))
+    	        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+					let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
+            	    guard d != 0 else { continue }
+                	let i = index(firstUnshuffled, offsetBy: d)
+                	swap(&self[firstUnshuffled], &self[i])
+            	}
+        	}
+    	}
+
+	#endif
 
 #endif
 
