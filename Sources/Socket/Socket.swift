cdfa5bc882e36ad6d0d685fa42fac1737188ab25
Fred Cox <mcfedr@gmail.com>
2018-08-30 15:07:41 +0300

    No need to use fallthrough in this switch

--- Sources/Socket/Socket.swift
+++ Sources/Socket/Socket.swift
@@ -3559,9 +3559,7 @@ public class Socket: SocketReader, SocketWriter {
 
 				// - Could be an error, but if errno is EAGAIN or EWOULDBLOCK (if a non-blocking socket),
 				//	it means there was NO data to read...
-				case EAGAIN:
-					fallthrough
-				case EWOULDBLOCK:
+				case EWOULDBLOCK, EAGAIN:
 					return self.readStorage.length
 
 				case ECONNRESET:
