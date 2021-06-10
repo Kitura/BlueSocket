9ee66feff29eadbd4788b3b0f4c16eb4e3c2641a
Danny Sung <danny@dannysung.com>
2021-06-10 10:41:58 -0700

    Ensure tools and dependencies only build for Swift 5.2+

--- Package.swift
+++ Package.swift
@@ -18,6 +18,64 @@
 
 import PackageDescription
 
+struct BuildInfo {
+    let product: [Product]
+    let dependencies: [Package.Dependency]
+    let targets: [Target]
+}
+
+
+let libraryBuildInfo = BuildInfo(
+    product: [
+        .library(
+            name: "Socket",
+            targets: ["Socket"]),
+        
+            .library(
+                name: "BlueSocketTestCommonLibrary",
+                targets: ["BlueSocketTestCommonLibrary"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Socket",
+            dependencies: [],
+            exclude: ["BlueSocket.xcodeproj", "BlueSocket.xcworkspace", "README.md", "Sources/Info.plist", "Sources/Socket.h"]
+        ),
+        .testTarget(
+            name: "SocketTests",
+            dependencies: ["Socket", "BlueSocketTestCommonLibrary"]
+        ),
+        
+        .target(
+            name: "BlueSocketTestCommonLibrary",
+            dependencies: [ "Socket" ]
+        ),
+    ]
+)
+
+let toolsBuildInfo = BuildInfo(
+    product: [
+        .executable(
+            name: "BlueSocketTestServer",
+            targets: ["BlueSocketTestServer"]),
+        .executable(
+            name: "BlueSocketTestClient",
+            targets: ["BlueSocketTestClient"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.1"),
+    ],
+    targets: [
+        .target(name: "BlueSocketTestServer",
+                dependencies: ["BlueSocketTestCommonLibrary", "ArgumentParser", ]
+               ),
+        .target(name: "BlueSocketTestClient",
+                dependencies: ["BlueSocketTestCommonLibrary", "ArgumentParser" ]
+               ),
+    ]
+)
+
 var products: [Product] = [
     .library(
         name: "Socket",
@@ -38,35 +96,21 @@ products.append(contentsOf: [
 ])
 #endif
 
+let buildInfo: BuildInfo
+#if swift(>=5.2)
+    buildInfo = BuildInfo(product: libraryBuildInfo.product + toolsBuildInfo.product,
+                          dependencies: libraryBuildInfo.dependencies + toolsBuildInfo.dependencies,
+                          targets: libraryBuildInfo.targets + toolsBuildInfo.targets)
+#else
+    buildInfo = BuildInfo(product: libraryBuildInfo.product, dependencies: libraryBuildInfo.dependencies, targets: libraryBuildInfo.targets)
+#endif
+
 #if os(Linux) || os(macOS) || os(iOS) || os(tvOS)
 let package = Package(
     name: "Socket",
-    products: products,
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.1.0"),
-    ],
-    targets: [
-        .target(
-            name: "Socket",
-            dependencies: [],
-            exclude: ["BlueSocket.xcodeproj", "BlueSocket.xcworkspace", "README.md", "Sources/Info.plist", "Sources/Socket.h"]
-        ),
-        .testTarget(
-            name: "SocketTests",
-            dependencies: ["Socket", "BlueSocketTestCommonLibrary"]
-        ),
-        
-        .target(
-            name: "BlueSocketTestCommonLibrary",
-            dependencies: [ "Socket" ]
-        ),
-        .target(name: "BlueSocketTestServer",
-                dependencies: ["BlueSocketTestCommonLibrary", "ArgumentParser", ]
-        ),
-        .target(name: "BlueSocketTestClient",
-                dependencies: ["BlueSocketTestCommonLibrary", "ArgumentParser" ]
-        ),
-    ]
+    products: buildInfo.product,
+    dependencies: buildInfo.dependencies,
+    targets: buildInfo.targets
 )
 #else
 fatalError("Unsupported OS")
